d2e99067a67b6dba969f66bec3bfbe7eda5ddb87
Rodrigo Junior <rodrigojunior0323@gmail.com>
2024-05-19 22:34:37 -0300

    Pesquisa melhorada.

--- app/src/main/java/com/example/matchmentor/view/ProfileAdapter.kt
+++ app/src/main/java/com/example/matchmentor/view/ProfileAdapter.kt
@@ -32,6 +32,7 @@ class ProfileAdapter(private var profiles: List<Profile>) : RecyclerView.Adapter
     class ProfileViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private val nameTextView: TextView = itemView.findViewById(R.id.name_text_view)
         private val ageTextView: TextView = itemView.findViewById(R.id.age_text_view)
+        private val cityTextView: TextView = itemView.findViewById(R.id.city_text_view)
         private val infoTextView: TextView = itemView.findViewById(R.id.info_text_view)
         private val photoImageView: ImageView = itemView.findViewById(R.id.photo_image_view)
 
@@ -39,9 +40,9 @@ class ProfileAdapter(private var profiles: List<Profile>) : RecyclerView.Adapter
             val fullName = "${profile.nome} ${profile.sobrenome}"
             nameTextView.text = fullName
             ageTextView.text = profile.idade?.toString() ?: "N/A"
+            cityTextView.text = "Cidade: ${profile.cidade}"
 
             val info = StringBuilder()
-            profile.cidade?.let { info.append("Cidade: $it\n") }
             profile.area_de_interesse?.let { info.append("Interesse: $it\n") }
             profile.profissao?.let { info.append("Profissão: $it\n") }
             profile.area_de_atuacao?.let { info.append("Área de Atuação: $it\n") }
@@ -49,9 +50,9 @@ class ProfileAdapter(private var profiles: List<Profile>) : RecyclerView.Adapter
 
             infoTextView.text = info.toString().trim()
 
+            val imageUrl = "https://focus-clientes.com.br/MatchMentorBackEnd/icons-users/${profile.foto}"
             if (!profile.foto.isNullOrEmpty()) {
-                val imageUrl = "https://focus-clientes.com.br/MatchMentorBackEnd/icons-users/${profile.foto}"
-                Glide.with(itemView.context).load(imageUrl).into(photoImageView)
+                Glide.with(itemView.context).load(imageUrl).centerCrop().into(photoImageView)
             } else {
                 photoImageView.setImageResource(R.drawable.image1)
             }
